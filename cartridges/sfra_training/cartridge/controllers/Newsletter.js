f7d9efbd58bfbf02c6e2269115afac1ffe103af3
Iryna Blotska <iryna_blotska@epam.com>
2022-04-12 00:35:26 +0300

    Display Data Submitted From a Form

--- cartridges/sfra_training/cartridge/controllers/Newsletter.js
+++ cartridges/sfra_training/cartridge/controllers/Newsletter.js
@@ -33,4 +33,39 @@ server.get(
     }
 );
 
+/**
+ * Newsletter-Handler : This endpoint is called to Handler the Newsletter form
+ * @name Newsletter-Handler
+ * @function
+ * @memberof Newsletter
+ * @param {middleware} - server.middleware.https
+ * @param {renders} - isml
+ * @param {serverfunction} - post
+ */
+server.post(
+    'Handler',
+    server.middleware.https,
+    function (req, res, next) {
+        var newsletterForm = server.forms.getForm('newsletter');
+        var continueUrl = dw.web.URLUtils.url('Newsletter-Show');
+
+        // Perform any server-side validation before this point, and invalidate form accordingly
+        if (newsletterForm.valid) {
+            // Send back a success status, and a redirect to another route
+            res.render('/newsletter/newslettersuccess', {
+                continueUrl: continueUrl,
+                newsletterForm: newsletterForm
+            });
+        } else {
+            // Handle server-side validation errors here: this is just an example
+            res.render('/newsletter/newslettererror', {
+                errorMsg: dw.web.Resource.msg('error.crossfieldvalidation', 'newsletter', null),
+                continueUrl: continueUrl
+            });
+        }
+
+        next();
+    }
+);
+
 module.exports = server.exports();
