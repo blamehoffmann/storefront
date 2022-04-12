acccfe1744e65d84c15f2459fa37dd3b7ec828a7
Iryna Blotska <iryna_blotska@epam.com>
2022-04-12 11:18:06 +0300

    Use Client Side JavaScript to Validate and Submit the Form

--- cartridges/sfra_training/cartridge/controllers/Newsletter.js
+++ cartridges/sfra_training/cartridge/controllers/Newsletter.js
@@ -5,6 +5,7 @@
  */
 
 var server = require('server');
+var URLUtils = require('dw/web/URLUtils');
 
 /**
  * Newsletter-Show : This endpoint is called to load the Newsletter page
@@ -63,17 +64,40 @@ server.post(
         // Perform any server-side validation before this point, and invalidate form accordingly
         if (newsletterForm.valid) {
             // Send back a success status, and a redirect to another route
-            res.render('/newsletter/newslettersuccess', {
-                continueUrl: continueUrl,
-                newsletterForm: newsletterForm
+            // res.render('/newsletter/newslettersuccess', {
+            //     continueUrl: continueUrl,
+            //     newsletterForm: newsletterForm
+            // });
+            // Show the success page
+            res.json({
+                success: true,
+                redirectUrl: URLUtils.url('Newsletter-Success').toString()
             });
         } else {
             // Handle server-side validation errors here: this is just an example
-            res.render('/newsletter/newslettererror', {
-                errorMsg: dw.web.Resource.msg('error.crossfieldvalidation', 'newsletter', null),
-                continueUrl: continueUrl
+            // res.render('/newsletter/newslettererror', {
+            //     errorMsg: dw.web.Resource.msg('error.crossfieldvalidation', 'newsletter', null),
+            //     continueUrl: continueUrl
+            // });
+            res.setStatusCode(500);
+            res.json({
+                error: true,
+                redirectUrl: URLUtils.url('Error-Start').toString()
             });
         }
+
+        next();
+    }
+);
+
+server.get(
+    'Success',
+    server.middleware.https,
+    function (req, res, next) {
+        res.render('/newsletter/newslettersuccess', {
+            continueUrl: URLUtils.url('Newsletter-Show'),
+            newsletterForm: server.forms.getForm('newsletter')
+        });
 
         next();
     }
