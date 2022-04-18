40f393a5ce7eb6171b1c363d4bdd5d9ffb41bcd7
Iryna Blotska <iryna_blotska@epam.com>
2022-04-18 19:33:51 +0300

    Use a Middleware Chain Event

--- cartridges/sfra_training/cartridge/controllers/Newsletter.js
+++ cartridges/sfra_training/cartridge/controllers/Newsletter.js
@@ -61,39 +61,41 @@ server.post(
 
         // Perform any server-side validation before this point, and invalidate form accordingly
         if (newsletterForm.valid) {
-            var Transaction = require('dw/system/Transaction');
-            try {
-                Transaction.wrap(function () {
-                    var CustomObjectMgr = require('dw/object/CustomObjectMgr');
-                    var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
-                    co.custom.firstName = newsletterForm.fname.value;
-                    co.custom.lastName = newsletterForm.lname.value;
+            this.on('route:BeforeComplete', function (req, res) {
+                var Transaction = require('dw/system/Transaction');
+                try {
+                    Transaction.wrap(function () {
+                        var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+                        var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
+                        co.custom.firstName = newsletterForm.fname.value;
+                        co.custom.lastName = newsletterForm.lname.value;
 
-                    res.json({
-                        success: true,
-                        redirectUrl: URLUtils.url('Newsletter-Success').toString()
+                        res.json({
+                            success: true,
+                            redirectUrl: URLUtils.url('Newsletter-Success').toString()
+                        });
                     });
-                });
-            } catch (e) {
-                var err = e;
-                if (err.javaName === 'MetaDataException') {
-                    /* Duplicate primary key on CO: send back message to client-side, but don't log error. This is possible if the user tries to subscribe with the same email multiple times */
-                    res.json({
-                        success: false,
-                        error: [Resource.msg('error.subscriptionexists', 'newsletter', null)]
-                    });
-                } else {
-                    /* Missing CO definition: Log error with message for site admin, set the response to error, and send error page URL to client-side */
-                    var Logger = require('dw/system/Logger');
-                    Logger.getLogger('newsletter_subscription').error(Resource.msg('error.customobjectmissing', 'newsletter', null));
-                    // Show general error page: there is nothing else to do
-                    res.setStatusCode(500);
-                    res.json({
-                        error: true,
-                        redirectUrl: URLUtils.url('Error-Start').toString()
-                    });
+                } catch (e) {
+                    var err = e;
+                    if (err.javaName === 'MetaDataException') {
+                        /* Duplicate primary key on CO: send back message to client-side, but don't log error. This is possible if the user tries to subscribe with the same email multiple times */
+                        res.json({
+                            success: false,
+                            error: [Resource.msg('error.subscriptionexists', 'newsletter', null)]
+                        });
+                    } else {
+                        /* Missing CO definition: Log error with message for site admin, set the response to error, and send error page URL to client-side */
+                        var Logger = require('dw/system/Logger');
+                        Logger.getLogger('newsletter_subscription').error(Resource.msg('error.customobjectmissing', 'newsletter', null));
+                        // Show general error page: there is nothing else to do
+                        res.setStatusCode(500);
+                        res.json({
+                            error: true,
+                            redirectUrl: URLUtils.url('Error-Start').toString()
+                        });
+                    }
                 }
-            }
+            });
         } else {
             // Handle server-side validation errors here: this is just an example
             res.json({
